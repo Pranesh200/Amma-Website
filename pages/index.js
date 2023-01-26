8a517000950c79740220dd98d6eb38c8dbac9b18
Pranesh Anand <pa6mr@virginia.edu>
2023-01-25 22:46:06 -0500

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,114 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react';
+
 
 const Home = () => {
+  const [userContext, setuserContext] = useState('');
+  const [userCode, setuserCode] = useState('');
+  const [userQuestion, setuserQuestion] = useState('');
+
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userContext, userCode, userQuestion }),
+    });
+
+    const data = await response.json();
+    console.log("Hi", data)
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  const onContextChangedText = (event) => {
+    console.log(event.target.value);
+    setuserContext(event.target.value);
+  };
+  const onCodeChangedText = (event) => {
+    console.log(event.target.value);
+    setuserCode(event.target.value);
+  };
+  const onQuestionChangedText = (event) => {
+    console.log(event.target.value);
+    setuserQuestion(event.target.value);
+  };
+
   return (
     <div className="root">
-      <Head>
-        <title>GPT-3 Writer | buildspace</title>
-      </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>StackGPT</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Context + Code + Question = Solution</h2>
           </div>
+          <div className="header-subtitle">
+          <h2>(Context and code are optional)</h2>
+          </div>
+
         </div>
+        <div className="prompt-container">
+          <textarea
+            className="prompt-box"
+            placeholder="provide some context to the code (a little context can go a long way!)"
+            value={userContext}
+            onChange={onContextChangedText}
+          />
+
+          <textarea
+            className="prompt-box"
+            placeholder="throw the code that needs fixing in here"
+            value={userCode}
+            onChange={onCodeChangedText}
+          />
+
+          <textarea
+            className="prompt-box"
+            placeholder="Enter your question here"
+            value={userQuestion}
+            onChange={onQuestionChangedText}
+          />
+          <div className="prompt-buttons">
+  <a
+    className={isGenerating ? 'generate-button loading' : 'generate-button'}
+    onClick={callGenerateEndpoint}
+  >
+    <div className="generate">
+    {isGenerating ? <span class="loader"></span> : <p> Generate solution </p>}
+    </div>
+  </a>
+</div>
+
+          {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+        </div>
+
+        
       </div>
+     
     </div>
   );
 };
