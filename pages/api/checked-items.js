1ce73c2ceee75d5227fd30a57ad7febebe5c8afc
Pranesh Anand <panand@costar.com>
2023-04-09 19:42:02 -0400

    adding quantity

--- pages/api/checked-items.js
+++ pages/api/checked-items.js
@@ -1,61 +1,72 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-    "https://bpnihamszdrqhtpvaizk.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwbmloYW1zemRycWh0cHZhaXprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxNDEzMjcsImV4cCI6MTk5MzcxNzMyN30.HbXoEwm4pYh8W6tStK-0Uz_yIArLRL6I75p1a8eKNhk"
+  "https://bpnihamszdrqhtpvaizk.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwbmloYW1zemRycWh0cHZhaXprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxNDEzMjcsImV4cCI6MTk5MzcxNzMyN30.HbXoEwm4pYh8W6tStK-0Uz_yIArLRL6I75p1a8eKNhk"
 );
 export default async function handler(req, res) {
-    const { customerName, customerPhone, checkedItems } = req.body;
+  const { customerName, customerPhone, checkedItems } = req.body;
 
-    try {
-        const orders = [];
+  try {
+    const orders = [];
+    const filteredCheckedItems = Object.fromEntries(
+      Object.entries(checkedItems).map(([key, value]) => [
+        key,
+        value.filter((item) => typeof item === "object"),
+      ])
+    );
 
-        // Loop through each day of the week in the checkedItems object
-        for (const dayOfWeek in checkedItems) {
-            const items = checkedItems[dayOfWeek];
+    console.log("HI", filteredCheckedItems);
+    // Loop through each day of the week in the checkedItems object
+    for (const dayOfWeek in filteredCheckedItems) {
+      const items = filteredCheckedItems[dayOfWeek];
 
-            // Loop through each item in the current day's items array
-            for (const itemName of items) {
-                // Query the menu table to get the menu_id for the current item
-                const { data: menuData, error: menuError } = await supabase
-                    .from('menu')
-                    .select('menu_id')
-                    .eq('item_name', itemName)
-                    .single();
+      // Loop through each item in the current day's items array
+      for (const item of items) {
+        console.log(item.itemName);
+        // Query the menu table to get the menu_id for the current item
+        const { data: menuData, error: menuError } = await supabase
+          .from("menu")
+          .select("menu_id")
+          .eq("item_name", item.itemName)
+          .single();
 
-                if (menuError) {
-                    console.error(menuError);
-                    return res.status(500).json({ message: 'Error inserting order' });
-                }
-
-                // If no matching item was found in the menu table, return an error
-                if (!menuData) {
-                    return res.status(400).json({ message: `Item "${itemName}" not found in menu` });
-                }
-
-                // Add a new order object to the orders array for the current item
-                orders.push({
-                    menu_id: menuData.menu_id,
-                    order_date: new Date(),
-                    customer_name: customerName,
-                    customer_phone: customerPhone
-                });
-            }
+        if (menuError) {
+          console.error(menuError);
+          return res.status(500).json({ message: "Error inserting order" });
         }
 
-        // Insert the new orders into the Supabase orders table
-        const { data: ordersData, error: ordersError } = await supabase
-            .from('orders')
-            .insert(orders);
-
-        if (ordersError) {
-            console.error(ordersError);
-            return res.status(500).json({ message: 'Error inserting order' });
+        // If no matching item was found in the menu table, return an error
+        if (!menuData) {
+          return res
+            .status(400)
+            .json({ message: `Item "${item.itemName}" not found in menu` });
         }
 
-        return res.status(200).json({ message: 'Orders successfully inserted' });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ message: 'Error inserting order' });
+        // Add a new order object to the orders array for the current item
+        orders.push({
+          menu_id: menuData.menu_id,
+          order_date: new Date(),
+          customer_name: customerName,
+          customer_phone: customerPhone,
+          quantity: item.quantity,
+        });
+      }
     }
+
+    // Insert the new orders into the Supabase orders table
+    const { data: ordersData, error: ordersError } = await supabase
+      .from("orders")
+      .insert(orders);
+
+    if (ordersError) {
+      console.error(ordersError);
+      return res.status(500).json({ message: "Error inserting order" });
+    }
+
+    return res.status(200).json({ message: "Orders successfully inserted" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: "Error inserting order" });
+  }
 }
--- pages/components/DayItems.jsx
+++ pages/components/DayItems.jsx
@@ -1,101 +1,192 @@
-import { Checkbox, CheckboxGroup, Stack, Button, Box, useToast, Spinner } from '@chakra-ui/react'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import {
+  Checkbox,
+  CheckboxGroup,
+  Stack,
+  Button,
+  Box,
+  useToast,
+  Spinner,
+  Flex,
+  Input,
+} from "@chakra-ui/react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function DayItems({ customerName, customerPhone }) {
-    const [menu, setMenu] = useState({});
-    const [loading, setLoading] = useState(false);
-    const [checkedItems, setCheckedItems] = useState({});
-    const allChecked = Object.values(checkedItems).flat()?.length === 21
-    const isIndeterminate = Object.values(checkedItems).flat()?.length > 0 && !allChecked
-    const toast = useToast();
+  const [menu, setMenu] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [checkedItems, setCheckedItems] = useState({});
+  const [quantityState, setQuantityState] = useState({});
+  const allChecked = Object.values(checkedItems).flat()?.length === 21;
+  const isIndeterminate =
+    Object.values(checkedItems).flat()?.length > 0 && !allChecked;
+  const toast = useToast();
 
-    useEffect(() => {
-        // Fetch the menu data from the API endpoint
-        setLoading(true)
-        const fetchMenu = async () => {
-            const res = await fetch("/api/menu");
-            const data = await res.json();
-            console.log("data", data);
-            setMenu(data);
-            setLoading(false)
+  useEffect(() => {
+    // Fetch the menu data from the API endpoint
+    setLoading(true);
+    const fetchMenu = async () => {
+      const res = await fetch("/api/menu");
+      const data = await res.json();
+      console.log("data", data);
+      setMenu(data);
+      setLoading(false);
+    };
+    fetchMenu();
+    const initialCheckedItems = {};
+    Object.keys(menu).forEach((day) => {
+      initialCheckedItems[day] = [];
+    });
+    setCheckedItems(initialCheckedItems);
+  }, []);
+
+  useEffect(() => {
+    console.log("CHECK", checkedItems);
+  }, [checkedItems]);
+
+  const handleCheckboxChange = (day, values) => {
+    console.log("HELLLLLo");
+    setCheckedItems((prevState) => ({
+      ...prevState,
+      [day]: values,
+    }));
+  };
+  const handleQuantityChange = (day, itemName, quantity) => {
+    setCheckedItems((prev) => {
+      const foundIndex = prev[day].findIndex(
+        (item) => item.itemName === itemName
+      );
+      if (foundIndex === -1) {
+        // item not found, add new item to the list
+        return {
+          ...prev,
+          [day]: [...prev[day], { itemName, quantity, isChecked: true }],
         };
-        fetchMenu();
-        const initialCheckedItems = {};
-        Object.keys(menu).forEach(day => {
-            initialCheckedItems[day] = [];
-        });
-        setCheckedItems(initialCheckedItems);
-    }, []);
+      } else {
+        // item found, update the properties
+        const updatedItems = [...prev[day]];
+        updatedItems[foundIndex] = {
+          ...updatedItems[foundIndex],
+          quantity,
+          isChecked: true,
+        };
+        return {
+          ...prev,
+          [day]: updatedItems,
+        };
+      }
+    });
+  };
 
+  const isChecked = (day, item) => {
+    console.log(
+      "HERE",
+      checkedItems[day]?.some((i) => i === item)
+    );
+    return checkedItems[day]?.some((i) => i === item);
+  };
 
+  const handleAllCheckboxChange = (day, checked) => {
+    const values = checked ? menu[day].map((item) => item.item_name) : [];
+    handleCheckboxChange(day, values);
+  };
 
-    const handleCheckboxChange = (day, values) => {
-        setCheckedItems(prevState => ({
-            ...prevState,
-            [day]: values
-        }))
+  const handleSubmit = async () => {
+    try {
+      const selectedItems = Object.entries(checkedItems).reduce(
+        (acc, [day, items]) => {
+          const selected = items.map((item) => ({
+            name: item,
+            quantity: quantityState[day]?.[item] || 1, // Use quantityState if available, otherwise default to 1
+          }));
+          return [...acc, ...selected];
+        },
+        []
+      );
+
+      const response = await axios.post("/api/checked-items", {
+        customerName,
+        customerPhone,
+        checkedItems,
+      });
+
+      toast({
+        title: "Submitted!",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+      console.log("submit", response);
+    } catch (error) {
+      console.log(customerName);
+      console.log(customerPhone);
+      console.log(checkedItems);
     }
+  };
 
-    const handleAllCheckboxChange = (day, checked) => {
-        const values = checked ? menu[day].map(item => item.item_name) : []
-        handleCheckboxChange(day, values)
-    }
-
-    const handleSubmit = async () => {
-        try {
-            console.log("HEREEEE")
-            const response = await axios.post('/api/checked-items', {
-                customerName,
-                customerPhone,
-                checkedItems
-            })
-            toast({
-                title: "Submitted!",
-                status: "success",
-                duration: 3000,
-                isClosable: true
-            });
-            console.log("submit", response)
-        } catch (error) {
-            console.log(customerName)
-            console.log(customerPhone)
-            console.log(checkedItems)
-        }
-    }
-
-
-    return (
-        <> {loading && <Spinner color="white"></Spinner>}
-            <Box display="flex" flexDirection="column">
-                {Object.entries(menu).map(([day, items]) => (
-                    <Box key={day} mb={4}>
-                        <CheckboxGroup value={checkedItems[day]} onChange={(values) => handleCheckboxChange(day, values)}>
-                            <Checkbox
-                                color="white"
-                                isChecked={checkedItems[day]?.length === items?.length}
-                                isIndeterminate={checkedItems[day]?.length > 0 && checkedItems[day]?.length < items?.length}
-                                onChange={(e) => handleAllCheckboxChange(day, e.target.checked)}
-                            >
-                                <span style={{ fontWeight: 'bold', textDecoration: 'underline' }}>{day}</span>
-                            </Checkbox>
-                            {items.length > 0 && (
-                                <Stack pl={6} mt={1} spacing={1}>
-                                    {items.map(item => (
-                                        <Checkbox key={item} value={item} color="white">
-                                            {item}
-                                        </Checkbox>
-                                    ))}
-                                </Stack>
-                            )}
-                        </CheckboxGroup>
-                    </Box>
-                ))}
-            </Box>
-
-            <Button mt={4} colorScheme="orange" onClick={handleSubmit}>Submit</Button>
-        </>
-    )
+  return (
+    <>
+      {" "}
+      {loading && <Spinner color="white"></Spinner>}
+      <Box display="flex" flexDirection="column">
+        {Object.entries(menu).map(([day, items]) => (
+          <Box key={day} mb={4}>
+            <CheckboxGroup
+              value={checkedItems[day]}
+              onChange={(values) => handleCheckboxChange(day, values)}>
+              <Checkbox
+                color="white"
+                isChecked={checkedItems[day]?.length === items?.length}
+                isIndeterminate={
+                  checkedItems[day]?.length > 0 &&
+                  checkedItems[day]?.length < items?.length
+                }
+                onChange={(e) =>
+                  handleAllCheckboxChange(day, e.target.checked)
+                }>
+                <span
+                  style={{
+                    fontWeight: "bold",
+                    textDecoration: "underline",
+                  }}>
+                  {day}
+                </span>
+              </Checkbox>
+              {items.length > 0 && (
+                <Stack pl={6} mt={1} spacing={1}>
+                  {items.map((item) => (
+                    <Flex key={item}>
+                      <Checkbox
+                        checked={isChecked(day, item)}
+                        value={item}
+                        color="white">
+                        {item}
+                      </Checkbox>
+                      <Input
+                        ml={2}
+                        placeholder="Quantity"
+                        type="number"
+                        color="white"
+                        default={1}
+                        min={1}
+                        max={10}
+                        onChange={(e) =>
+                          handleQuantityChange(day, item, e.target.value)
+                        }
+                      />
+                    </Flex>
+                  ))}
+                </Stack>
+              )}
+            </CheckboxGroup>
+          </Box>
+        ))}
+      </Box>
+      <Button mt={4} colorScheme="orange" onClick={handleSubmit}>
+        Submit
+      </Button>
+    </>
+  );
 }
 
-export default DayItems
+export default DayItems;
