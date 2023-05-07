161659ab7739247496d44df504609d653c71c7c9
Pranesh Anand <panand@costar.com>
2023-05-06 21:44:14 -0400

    bug fixes

--- pages/components/MenuBuilder.jsx
+++ pages/components/MenuBuilder.jsx
@@ -1,4 +1,12 @@
-import { VStack, Input, useToast, Box, Button, Heading, Select } from "@chakra-ui/react";
+import {
+  VStack,
+  Input,
+  useToast,
+  Box,
+  Button,
+  Heading,
+  Select,
+} from "@chakra-ui/react";
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import AlertPop from "./AlertPop";
@@ -6,108 +14,127 @@ import DayItems from "./DayItems";
 import Stats from "./Stats";
 import { createClient } from "@supabase/supabase-js";
 
-
 export default function MenuBuilder() {
-    // Initialize a new Supabase client
-    const supabase = createClient(
-        "https://bpnihamszdrqhtpvaizk.supabase.co",
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwbmloYW1zemRycWh0cHZhaXprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxNDEzMjcsImV4cCI6MTk5MzcxNzMyN30.HbXoEwm4pYh8W6tStK-0Uz_yIArLRL6I75p1a8eKNhk"
-    );
+  // Initialize a new Supabase client
+  const supabase = createClient(
+    "https://bpnihamszdrqhtpvaizk.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwbmloYW1zemRycWh0cHZhaXprIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgxNDEzMjcsImV4cCI6MTk5MzcxNzMyN30.HbXoEwm4pYh8W6tStK-0Uz_yIArLRL6I75p1a8eKNhk"
+  );
 
-    // Define the data to be inserted into the menu table
-    const sampleData = {
-        item_name: "Cheeseburger",
-        day_of_week: "Monday",
+  // Define the data to be inserted into the menu table
+  const sampleData = {
+    item_name: "Cheeseburger",
+    day_of_week: "Monday",
+  };
+
+  const toast = useToast();
+  const [data, setData] = useState();
+  const mystyle = {
+    gap: "64px",
+  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    const insertData = {
+      item_name: data.item_name,
+      day_of_week: data.day_of_week,
+      item_quantity: data.item_quantity,
+      start_date: new Date(Date.now()),
+      end_date: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     };
 
-    const toast = useToast();
-    const [data, setData] = useState();
-    const mystyle = {
-        gap: "64px"
-    };
-    const {
-        register,
-        handleSubmit,
-        formState: { errors }
-    } = useForm();
-    const onSubmit = (data) => {
-        const insertData = {
-            item_name: data.item_name,
-            day_of_week: data.day_of_week,
-            start_date: new Date(Date.now()),
-            end_date: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
-        };
+    //        Use the Supabase client to insert the data into the menu table
+    supabase
+      .from("menu")
+      .insert(insertData)
+      .then((result) => {
+        console.log("RESULT", result);
+      })
+      .catch((error) => {
+        console.log("ERROR", error);
+      });
+    console.log(data.item_name);
+    toast({
+      title: "Submitted!",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+    });
 
-        //        Use the Supabase client to insert the data into the menu table
-        supabase
-            .from("menu")
-            .insert(insertData)
-            .then((result) => {
-                console.log("RESULT", result);
-            })
-            .catch((error) => {
-                console.log("ERROR", error);
-            });
-        console.log(data.item_name);
-        toast({
-            title: "Submitted!",
-            status: "success",
-            duration: 3000,
-            isClosable: true
-        });
+    setData(data);
+  };
+  console.log(data);
+  console.log(errors);
 
-        setData(data);
-    };
-    console.log(data);
-    console.log(errors)
-
-    return (
-        <Box>
-            <div style={mystyle}>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <VStack mb={20}>
-                        <Heading as="h4" fontSize="20px" color='white'>
-                            Item Name:
-                        </Heading>
-                        <Input
-                            type="text"
-                            color='white'
-                            _placeholder={{ color: 'inherit' }}
-
-                            placeholder="Item Name"
-                            {...register("item_name", {
-                                required: "Please enter item name",
-                                minLength: 3,
-                                maxLength: 100
-                            })}
-                        />
-                        <Heading as="h4" fontSize="20px" color='white'>
-                            Item Day:
-                        </Heading>
-                        {errors.lastname && <AlertPop title={errors.lastname.message} />}
-                        <Select bg='white' color='black' placeholder='Select day of the week' {...register("day_of_week", {
-                            required: "Please select a day of the week"
-                        })}>
-                            <option value='Monday'>Monday</option>
-                            <option value='Tuesday'>Tuesday</option>
-                            <option value='Wednesday'>Wednesday</option>
-                            <option value='Thursday'>Thursday</option>
-                            <option value='Friday'>Friday</option>
-                            <option value='Saturday'>Saturday</option>
-                            <option value='Sunday'>Sunday</option>
-                        </Select>
-                        <Button
-                            borderRadius="md"
-                            bg="rgb(255, 79, 18)"
-                            _hover={{ bg: "cyan.200" }}
-                            variant="ghost"
-                            type="submit"
-                        >
-                            Submit
-                        </Button>
-                    </VStack>
-                </form>
-            </div>
-        </Box>
-    );
+  return (
+    <Box>
+      <div style={mystyle}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <VStack mb={20}>
+            <Heading as="h4" fontSize="20px" color="white">
+              Item Name:
+            </Heading>
+            <Input
+              type="text"
+              color="white"
+              _placeholder={{ color: "inherit" }}
+              placeholder="Item Name"
+              {...register("item_name", {
+                required: "Please enter item name",
+                minLength: 3,
+                maxLength: 100,
+              })}
+            />
+            <Heading as="h4" fontSize="20px" color="white">
+              Item Day:
+            </Heading>
+            {errors.lastname && <AlertPop title={errors.lastname.message} />}
+            <Select
+              bg="white"
+              color="black"
+              placeholder="Select day of the week"
+              {...register("day_of_week", {
+                required: "Please select a day of the week",
+              })}>
+              <option value="Monday">Monday</option>
+              <option value="Tuesday">Tuesday</option>
+              <option value="Wednesday">Wednesday</option>
+              <option value="Thursday">Thursday</option>
+              <option value="Friday">Friday</option>
+              <option value="Saturday">Saturday</option>
+              <option value="Sunday">Sunday</option>
+            </Select>
+            <Heading as="h4" fontSize="20px" color="white">
+              Item Quantity:
+            </Heading>
+            <Input
+              ml={2}
+              placeholder="Quantity"
+              type="number"
+              color="white"
+              default={1}
+              min={1}
+              max={10}
+              pattern="^[0-9]*$"
+              inputMode="numeric" // Added this line to restrict input to only numbers
+              {...register("item_quantity", {
+                required: "Please select a day of the week",
+              })}
+            />
+            <Button
+              borderRadius="md"
+              bg="rgb(255, 79, 18)"
+              _hover={{ bg: "cyan.200" }}
+              variant="ghost"
+              type="submit">
+              Submit
+            </Button>
+          </VStack>
+        </form>
+      </div>
+    </Box>
+  );
 }
